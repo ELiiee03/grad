88cf2b2b6fc3ecbf52a76bb97e5eff498ade5c0c
Lieca Eleccion <eleccionlieca03@gmail.com>
2024-05-30 18:38:27 -0800

    preloader not final

---
+++ script.js
@@ -0,0 +1,12 @@
+// // script.js
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Hide the preloader once the content is loaded
+//     var preloader = document.getElementById('preloader');
+//     var content = document.getElementById('content');
+    
+//     window.onload = function() {
+//         preloader.style.display = 'none';
+//         content.style.display = 'block';
+//     };
+// });
