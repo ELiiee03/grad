00adf86e8d0b94ee26a98e37a177b9a215998d1e
Lieca Eleccion <eleccionlieca03@gmail.com>
2024-05-31 02:37:41 -0800

    debuuggg

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 // // script.js
 
+// script.js
+
 // document.addEventListener("DOMContentLoaded", function() {
-//     // Hide the preloader once the content is loaded
 //     var preloader = document.getElementById('preloader');
 //     var content = document.getElementById('content');
     
 //     window.onload = function() {
-//         preloader.style.display = 'none';
-//         content.style.display = 'block';
+//         preloader.style.opacity = '0'; // Fade out the preloader
+
+//         // Wait for the transition to complete before hiding the preloader
+//         setTimeout(function() {
+//             preloader.style.display = 'none';
+//             // content.style.display = 'block'; // Show the content
+//         }, 2000); // This should match the duration of the CSS transition (2s)
 //     };
 // });
+
+const preloaderEl = document.querySelector('.preloader');
+
+window.addEventListener('load', function() {
+
+    // preloaderEl.classList.add('fade-out-animation')
+    setTimeout(() => {
+        preloaderEl.style.display = 'none';
+    }, 3000);
+
+    preloaderEl.style.opacity = '1';
+});
+   
+// setTimeout(() => {
+//     // preloaderEl.classList.add('hide');
+//     preloaderEl.style.opacity = '0'
+// }, 2000);
